01dc1cdff5b2d81eed785cf4fd7195203feba3b6
TotemSIN <74totem.sin@gmail.com>
2025-01-21 15:45:47 +0100

    Create socket_server.py
    Test socket server py

---
+++ Codes/socket_server.py
@@ -0,0 +1,14 @@
+import socket
+
+s = socket.socket()
+host = "localhost"
+port = 12345
+s.bind((host, port))
+
+s.listen(5)
+while True:
+   c, addr = s.accept()
+   data = c.recv(1024)
+   if data: 
+       print(data)
+   c.close()
